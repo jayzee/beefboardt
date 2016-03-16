c9ea681c8a19c1fe9f84d0ee78409dfe79b41254
lizkalina <liz.kalina@gmail.com>
2016-03-16 09:49:28 -0400

    adds devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@
 #
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :attendees
   has_many :hosts
   # has_many :attended_events, :class_name => "Event", through: :attendees
